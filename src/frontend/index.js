9566c1f0a01e13cb73b10fb24ecd50a4d020b91b
MakiElric <waifuism4ever@gmail.com>
2020-01-02 21:07:23 -0600

    Configurando el entorno de trabajo (Webpack, eslint, prettier, react, sass, babel)

---
+++ src/frontend/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloMaki from './components/HelloMaki';
+
+ReactDOM.render(<HelloMaki />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/frontend/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              resources: [
+                './src/frontend/assets/styles/base/variables.scss',
+                './src/frontend/assets/styles/base/placeholder.scss',
+                './src/frontend/assets/styles/base/mixin.scss',
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/app.css',
+    }),
+  ],
+};
